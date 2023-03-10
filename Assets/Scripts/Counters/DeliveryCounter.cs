36969349159e44f050a8cd9f5e58e4313bbb5748
PabloCS <titanbenevolo@icloud.com>
2023-03-10 14:57:12 -0300

    Base delivery counter logic.

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(PlayerController player)
+    {
+        if (player.HasKitchenObject())
+        {
+            if (player.GetKitchenObj().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                player.GetKitchenObj().DestroySelf();
+            }
+        }
+    }
+}
